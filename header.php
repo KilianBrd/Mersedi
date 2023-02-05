630acb61fbb8983c10984a4538bd2df4b5260e75
Kilian Beraud <kilianberaud3@gmail.com>
2023-02-05 20:16:32 +0100

    Login fonctionnel

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+<nav>
+		<ul>
+			<li>
+				<a id="navLien" href="index.php">Accueil</a>
+			</li>
+			<li>
+				<a id="navLien" href="contact.php">Contacte-moi</a>
+			</li>
+			<li>
+				<a id="navLien" href="connection.php">Se connecter</a>
+			</li>
+			<li>
+				<a id="navLien" href="inscription.php">S'inscrire</a>
+			</li>
+			<li>
+				<?php echo '<p id="pseudoNav">' . $_SESSION['pseudo'] . "</p>"; ?>
+			</li>
+		</ul>
+	</nav>
+</body>

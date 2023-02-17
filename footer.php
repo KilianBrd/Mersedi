1e19f8eebc4c05aba8bce12b0d90820a76e33950
Kilian Beraud <kilianberaud3@gmail.com>
2023-02-17 08:50:22 +0100

    footer avec email correct

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="contact">
         <h5>Contact</h5>
         <div class="contact_img">
-            <a href="mailto:email@example.com"><img src="./assets/gmail.png"></a>
+            <a href="mailto:mersedifootvelo@gmail.com"><img src="./assets/gmail.png"></a>
             <a href="https://discord.gg/RXDJk9PPQg"><img src="./assets/discordlogo.png"></a>
         </div>
     </div>

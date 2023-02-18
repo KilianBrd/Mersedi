a2cecf454a6d2ce4c38cabc095d31abdabb29490
flojucv <flojucv@gmail.com>
2023-02-18 03:02:38 +0100

    Ajout d'un style a la page de connection Reste encore a régler le problème du footer
    Changes to be committed:
    	modified:   connection.php
    	modified:   footer.php
    	modified:   style/style.css

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
         <h1> Connecte-toi !</h1>
         <input type="text" name="pseudo" id="pseudo" placeholder="Ton pseudo !" required><br>
         <input type="password" name="mdp" id="mdp" placeholder="Ton mot de passe secret" required><br>
-        <input type="submit" name="formsend" id="formsend" action="/Mersedi/backend/connectionBackEnd.php">
+        <input type="submit" name="formsend" id="formsend" class="custom-btn btn_connexion">
         <a href="./index.php">Mot de passe oubliez ?</a>
     </form>
 </div>
